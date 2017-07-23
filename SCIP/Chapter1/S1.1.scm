c8551580c7e511b11ef24f2b6f1fc3ae0617e282
Shusei-E <shusei.eshima@gmail.com>
2017-07-23 22:48:52 +0900

    170723

--- SCIP/Chapter1/S1.1.scm
+++ SCIP/Chapter1/S1.1.scm
@@ -6,3 +6,16 @@
         ((< x 0) (- x))
   )
 )
+
+(define (abs2 x)
+  (cond ((< x 0) (- x))
+        (else x)
+        )
+  )
+(abs2 -1)
+
+(define (absc x)
+  (if (< x 0) (- x) x) 
+  )
+(absc 5)
+
