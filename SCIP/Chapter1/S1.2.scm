46dde7a65b2769578e50d4b8a54139babc9c8626
Shusei-E <shusei.eshima@gmail.com>
2017-08-19 18:56:22 +0900

    170819

---
+++ SCIP/Chapter1/S1.2.scm
@@ -0,0 +1,22 @@
+; 1.2
+(define  (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))
+      )
+  )
+(factorial 3)
+
+(define (factorial n)
+  (fact-iter 1 1 n)
+  )
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count
+                 )
+      )
+  )
+
